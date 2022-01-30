8dcd8842b326155c2921dfc9030aebd99c45911c
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-01-30 14:49:53 +0100

    feat(model): define the application models

--- backend/prisma/db/seed.ts
+++ backend/prisma/db/seed.ts
@@ -1,6 +1,20 @@
 import prisma from './client';
 
-const genreData = ['Mobile', 'Desktop', 'Web'];
+const genreData = ['Desktop', 'Framework', 'Library', 'Mobile', 'Multiplatform', 'Video Game', 'Web Application'];
+const categoryData = [
+  'Agriculture',
+  'Art',
+  'Cinema',
+  'E-commerce',
+  'Education',
+  'Finance',
+  'Health',
+  'Music',
+  'Other',
+  'Social',
+  'Tourism',
+  'Tech',
+];
 
 const seedGenre = async () => {
   const promises = genreData.map((genre) =>
@@ -10,8 +24,17 @@ const seedGenre = async () => {
   return Promise.all(promises);
 };
 
+const seedCategory = async () => {
+  const promises = categoryData.map((category) =>
+    prisma.category.upsert({ create: { name: category }, update: { name: category }, where: { name: category } }),
+  );
+
+  return Promise.all(promises);
+};
+
 const seedDatabase = async () => {
   await seedGenre();
+  await seedCategory();
 };
 
 export { seedDatabase };
--- backend/src/types/model.ts
+++ backend/src/types/model.ts
@@ -1,1 +1,50 @@
-type Application = {};
+/**
+ * Model Genre
+ *
+ */
+export type Genre = {
+  createdAt: Date;
+  id: string;
+  name: string;
+  updatedAt: Date;
+};
+
+/**
+ * Model Category
+ *
+ */
+export type Category = {
+  createdAt: Date;
+  id: string;
+  name: string;
+  updatedAt: Date;
+};
+
+/**
+ * Model Application
+ *
+ */
+export type Application = {
+  appstoreUrl: string | null;
+  categoryId: string;
+  createdAt: Date;
+  description: string | null;
+  dikaloUrl: string | null;
+  facebookUrl: string | null;
+  genreId: string;
+  githubUrl: string | null;
+  id: string;
+  linkedinUrl: string | null;
+  logo: string | null;
+  name: string;
+  othersUrl: string[];
+  pictureUrl: string | null;
+  playstoreUrl: string | null;
+  slackUrl: string | null;
+  tags: string[];
+  telegramUrl: string | null;
+  twitterUrl: string | null;
+  updatedAt: Date;
+  websiteUrl: string | null;
+  whatsappUrl: string | null;
+};
