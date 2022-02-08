2d8ab7a97254bd14a58b914b9319bfdee9f3e7e7
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-02-08 23:46:43 +0100

    docs(application): update the application doc to include pagination

--- backend/src/docs/application/findall-application.ts
+++ backend/src/docs/application/findall-application.ts
@@ -3,6 +3,15 @@ import { applicationListResponseSchema } from '../response';
 const findAllApplication = {
   description: 'Retrieve all applications',
   operationId: 'getAllApplications',
+  parameters: [
+    {
+      description: 'The token where to start retrieving the data',
+      example: '1644272953769',
+      in: 'query',
+      name: 'nextToken',
+      required: false,
+    },
+  ],
   responses: {
     '200': {
       content: {
